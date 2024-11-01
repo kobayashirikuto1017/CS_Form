1f2135ec6681d2ef79ac0ac39794477a197bc877
DESKTOP-BJ2LRPE\User <riku.koba1017@gmail.com>
2024-11-01 17:06:26 +0900

    ボタン　10個

--- CS_Form/CS_Form/Program.cs
+++ CS_Form/CS_Form/Program.cs
@@ -18,5 +18,7 @@ namespace CS_Form
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+
     }
+
 }
--- CS_Form/CS_Form/test.cs
+++ CS_Form/CS_Form/test.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,17 +14,23 @@ namespace CS_Form
         //<summary>
         //コンストラクタ
         //クラスを生成した時に呼び出される
-        public TestButton()
+        public TestButton(int  x, int y,int Width,int Height)
         {
             //clickイベントにOnClick関数を登録
             //ボタンをクリックしたときに登録した関数を実行します
             Click += OnClick;
+
+               Text = "ボタン";
+
+            Location = new Point(x,y);
+
+            Size = new Size(Width, Height);
         }
         //<summary>
         //ボタンをクリックした才の処理を記述する
         //＜/summary>
         
-
+          
 
         public void OnClick(object sender, EventArgs e)
         {
