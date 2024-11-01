cc9fdb5fc426cbb2cc122261b2ef66cfb29e9eec
DESKTOP-BJ2LRPE\User <riku.koba1017@gmail.com>
2024-11-02 01:36:51 +0900

    ボタンの数字とクリックしたときの数字を同じにしました

---
+++ 新しいフォルダー/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_Form
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+
+            {
+                TestButton testButton = new TestButton(i, (i % 5) * 100, (i / 5) * 100, 100, 100);
+                Controls.Add(testButton);
+            }
+        }
+    }
+}
